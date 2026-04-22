074a66b03df8a2fed245e096a7e1419f6d8c6a60
nguyenvotrungkien <nguyenvotrungkien7002@gmail.com>
2026-04-22 12:55:25 +0700

    Delete bai 1.sql

--- bai 1.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE products_management;
-USE products_management;
-CREATE TABLE Products (
-    ID INT PRIMARY KEY ,
-    ProductName VARCHAR(100) ,
-    Price DECIMAL(18, 2) ,
-    Description text
-);
-select * from Products;
